cb910521f453f180dc687ee46aedfcdc3277ba03
Alejandro <alejoft13@gmail.com>
2022-05-27 20:56:13 -0500

    cambios bd

--- database/config.db.js
+++ database/config.db.js
@@ -6,6 +6,9 @@ const dbConnection = async () => {
 
         mongoose.connect(process.env.MONGODB_ATLAS, {
             useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
         });
 
         console.log("Base de datos online")
